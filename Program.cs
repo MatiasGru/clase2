cf597ad3066226f10d52b42589efa289339e5b6d
matu gruning <you@example.com>
2021-08-24 20:12:12 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clase_de_hoy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
